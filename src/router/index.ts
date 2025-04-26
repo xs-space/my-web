65495209c81bffadb0e015684b21954c8b12bcef
xs <w18320724197@163.com>
2025-04-26 11:33:13 +0800

    build: 项目搭建

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,18 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router"
 
 const routes: RouteRecordRaw[] = [
-  { path: "/", redirect: "/home" },
-  { path: "/home", component: () => import("@/views/Home.vue") },
-  { path: "/about", component: () => import("@/views/About.vue") }
+  {
+    path: "/",
+    redirect: "/home"
+  },
+  {
+    path: "/home",
+    component: () => import("@/views/Home.vue")
+  },
+  {
+    path: "/about",
+    component: () => import("@/views/About.vue")
+  }
 ]
 
 export default createRouter({
