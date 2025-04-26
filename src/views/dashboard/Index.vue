5edec207d28cfd338f7650fd3828150ea37514f2
xs <w18320724197@163.com>
2025-04-26 18:38:19 +0800

    build: 项目搭建

--- src/views/dashboard/Index.vue
+++ src/views/dashboard/Index.vue
@@ -1,15 +1,14 @@
 <template>
   <el-button type="primary">按钮</el-button>
-  <HelloWorld msg="hi"></HelloWorld>
-  <el-button @click="handle"></el-button>
+  <div></div>
 </template>
 
 <script lang="ts" setup>
-const { proxy } = getCurrentInstance()!
+// const { proxy } = getCurrentInstance()!
 
-const a = ref(0)
-console.log(a.value)
-const handle = () => {
-  proxy?.$message("This is a message")
-}
+// const a = ref(0)
+// console.log(a.value)
+// const handle = () => {
+//   proxy?.$message("This is a message")
+// }
 </script>
--- uno.config.ts
+++ uno.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "unocss"
 import presetAttributify from "@unocss/preset-attributify"
 import presetUno from "@unocss/preset-uno"
 import transformDirective from "@unocss/transformer-directives"
+import presetIcons from "@unocss/preset-icons"
 
 export default defineConfig({
-  presets: [presetAttributify(), presetUno()],
+  presets: [presetAttributify(), presetUno(), presetIcons()],
   transformers: [transformDirective()], // apply
   shortcuts: [["icon", "inline-block w-1em h-1em align-middle text-current"]]
 })
