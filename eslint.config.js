52b612adff7b81a10d877d21863052566a58a8ef
xs <w18320724197@163.com>
2025-04-26 10:09:50 +0800

    项目搭建

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,22 @@
-import js from "@eslint/js"; // 校验js规范
-import globals from "globals";
-import tseslint from "typescript-eslint"; // 校验ts规范
-import pluginVue from "eslint-plugin-vue";
-import { defineConfig } from "eslint/config";
+import js from "@eslint/js" // 校验js规范
+import globals from "globals"
+import tseslint from "typescript-eslint" // 校验ts规范
+import pluginVue from "eslint-plugin-vue"
+import { defineConfig } from "eslint/config"
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,vue}"],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ["js/recommended"]
   },
   {
     files: ["**/*.{js,mjs,cjs,ts,vue}"],
     languageOptions: {
       globals: {
         ...globals.browser,
-        ...globals.node,
-      },
+        ...globals.node
+      }
     }
   },
   tseslint.configs.recommended,
@@ -25,14 +25,14 @@ export default defineConfig([
     files: ["**/*.vue"], // 校验vue中的ts代码
     languageOptions: {
       parserOptions: {
-        parser: tseslint.parser,
-      },
-    },
+        parser: tseslint.parser
+      }
+    }
   },
   {
     // 自定义规则,根据需要增加  eslint 主要是校验代码规范  prettier  格式化代码的
     rules: {
-      "no-console": "warn",
-    },
-  },
-]);
+      "no-console": "warn"
+    }
+  }
+])
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
+import { createApp } from "vue"
+import "./style.css"
+import App from "./App.vue"
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.mount("#app");
-
-let a = 1
+app.mount("#app")
