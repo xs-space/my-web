59e2fc222a71b6922c70d75ce79e0457100f9ad7
xs <w18320724197@163.com>
2025-04-26 09:52:53 +0800

    项目搭建

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import js from "@eslint/js"; // 校验js规范
+import globals from "globals";
+import tseslint from "typescript-eslint"; // 校验ts规范
+import pluginVue from "eslint-plugin-vue";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,vue}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,vue}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    }
+  },
+  tseslint.configs.recommended,
+  pluginVue.configs["flat/essential"],
+  {
+    files: ["**/*.vue"], // 校验vue中的ts代码
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
+  // {
+  //   // 自定义规则,根据需要增加  eslint 主要是校验代码规范  prettier  格式化代码的
+  //   rules: {
+  //     "no-console": "warn",
+  //   },
+  // },
+]);
