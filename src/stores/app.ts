dfc78cb49cd66ffc0980d720b2cee505f177fd9f
xs <w18320724197@163.com>
2025-04-26 23:02:37 +0800

    build: 项目搭建

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -1,14 +1,23 @@
-export const useAppStore = defineStore("app", () => {
-  const state = reactive({
-    sidebar: {
-      opend: true
+export const useAppStore = defineStore(
+  "app",
+  () => {
+    const state = reactive({
+      sidebar: {
+        opend: true
+      }
+    })
+
+    const sidebar = computed(() => state.sidebar)
+    const toggleSidebar = () => {
+      state.sidebar.opend = !state.sidebar.opend
     }
-  })
 
-  const sidebar = computed(() => state.sidebar)
-  const toggleSidebar = () => {
-    state.sidebar.opend = !state.sidebar.opend
+    return { state, sidebar, toggleSidebar }
+  },
+  {
+    persist: {
+      storage: window.localStorage,
+      pick: ["state.sidebar"]
+    }
   }
-
-  return { sidebar, toggleSidebar }
-})
+)
